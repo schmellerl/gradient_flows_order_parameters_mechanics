7e480ad4385f3ca7bdd0597cf9205ca18b1e0e9f
schmellerl <65337849+schmellerl@users.noreply.github.com>
2022-03-09 12:25:13 +0100

    Update model_parameters.py

--- colab/Data/model_parameters.py
+++ colab/Data/model_parameters.py
@@ -10,10 +10,10 @@ gamma3  = 2.24     # surface tension air
 m       = 1e-6     # Cahn-Hilliard mobility
 mu      = 1        # Solid Stokes viscosity 
 
-h0T   = 1          # substrate height 
-h1T   = 0.36       # fluid height
+h0T     = 1        # substrate height 
+h1T     = 0.36     # fluid height
 
-eps   = 0.5e-2     # interface width
+eps     = 0.5e-2   # interface width
 
 interface_factor =  Constant(1.0/sqrt(2))
 
