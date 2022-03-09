2fd0688207a268186f8bea121ac9050f2d488f6b
schmellerl <65337849+schmellerl@users.noreply.github.com>
2022-03-09 13:30:42 +0100

    Update Neumann_construction.py

--- colab/Data/Neumann_construction.py
+++ colab/Data/Neumann_construction.py
@@ -9,8 +9,8 @@ def Neumann_construction(theta_in_liquid, theta_in_solid, gamma_sa ):
   theta_in_liquid = theta_in_liquid*pi/180   
   theta_in_solid  = theta_in_liquid*pi/180
 
-  theta_2 = 180*pi/180 - theta_outer 
-  theta_1 = theta_inner - theta_2
+  theta_2 = 180*pi/180 - theta_in_solid 
+  theta_1 = theta_in_liquid - theta_2
 
   gamma_la = gamma_sa * 1/(cos(theta_1)+cos(theta_2)*sin(theta_1)/sin(theta_2) )
   gamma_sl = sin(theta_1)/sin(theta_2) * gamma_la
